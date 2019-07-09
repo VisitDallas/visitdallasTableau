7513d8555bac7515ef98a8cf776982fd9bd9fcbf
Nick Askam <asknickfilms@gmail.com>
2019-07-09 15:31:15 -0500

    adding quotes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render(home);
+    res.render("home");
 });
 
 app.listen(process.env.PORT, process.env.IP);
